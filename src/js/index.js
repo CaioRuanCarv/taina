949339986958a4e0e3ad071868bda24f791f4b4a
Caio Ruan <caiioruancarv@gmail.com>
2023-02-14 23:35:49 -0300

    primeira vez

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+document.getElementById("btn").addEventListener('click',()=>{
+    document.getElementById("cartao").classList.add('select')
+    document.getElementById("cartao").innerHTML +=` <p>Oi meu amor, hoje é um dia especial, você finalmente alcançou seus 18 aninhos e esse é um grande marco na sua história.
+    <br>
+    <br>
+    Eu sei que como você deve está com medo do futuro que lhe aguarda e das quests que você vai ter que realizar, como também de acabar tomando decisões precipitadas.
+    <br><br>
+     É complicado pensar que agora o nosso futouro está completamente nas nossas mãos,carregar seu futuro é uma grande responsabilidade, mas sabe tudo depende do ângulo como vemos as coisas, porque você ser a dona do seu próprio futuro lhe permite fazer o que quiser com ele.
+    <br>
+    <br>
+     Você pode manipular o seu futuro a maneira que você desejar, seguir os caminhos que quiser seguir, mas não mudando o fato de que haverão caminhos mais fáceis ou mais difíceis do que outros, mas eu sei que não importa qual seja a quest que você tenha que concluir ou o chefão que você tenha que derrotar. <br><br>
+     <img src="src/images/Chefão.jpeg"><br><br>
+     Eu sei que você é capaz, sabe tainá eu posso não ser o namorado perfeito, posso não ser a pessoa que sempre sabe o que fazer, mas saiba que eu vou sempre dar o meu melhor para poder te apoiar no que você precisar.<br><br>
+     Vou ser a espada que vai te ajudar nas quests mais difíceis e eu sei que sempre que eu precisar você também vai está lá, por isso tainá você não está sozinha, não importa o quão grande seja a dificuldade eu vou está lá para te ajudar.<br><br>
+    <img src="src/images/casal.jpeg"> </p>`
+})
